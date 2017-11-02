efcb131f9145a229b16194b55f501ef892bbb189
stvenyin <893692456@qq.com>
2017-11-03 00:18:20 +0800

    example3

---
+++ example/example3.go
@@ -0,0 +1,14 @@
+// test project main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	array := [8]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}
+	fmt.Println([...]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}[1])
+	fmt.Println(len([8]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}))
+	fmt.Println(array[3])
+}
