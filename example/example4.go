1df44cd42e15448e846201750a397ec764e15484
stvenyin <893692456@qq.com>
2017-11-05 00:19:22 +0800

    exampl4

---
+++ example/example4.go
@@ -0,0 +1,23 @@
+// test project main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println(len([]string{"Go", "Python", "Java", "C", "C++", "PHP"}))
+	fmt.Println(cap([]string{"Go", "Python", "Java", "C", "C++", "PHP"}))
+	fmt.Println([]string{"Go", "Python", "Java", "C", "C++", "PHP"}[5])
+
+	array := [...]string{"Go", "Python", "java", "C", "C++", "PHP"}
+	slice1 := array[:]
+	slice1 = append(slice1, "1", "2")
+	slice2 := array[3:]
+	slice1[0] = "slice1"
+	fmt.Println(slice1)
+	fmt.Println(slice2)
+	fmt.Println(array)
+
+}
