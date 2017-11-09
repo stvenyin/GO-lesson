29a0e77095863ebc7788ebfa58ffe9e21d5fde92
stvenyin <893692456@qq.com>
2017-11-09 23:18:25 +0800

    example6

---
+++ example/example6.go
@@ -0,0 +1,30 @@
+// test project main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	num := 123
+	switch {
+	case num < 100:
+		num++
+	case num < 200:
+		num--
+	default:
+		num -= 2
+	}
+	content := "go"
+	fmt.Println(num)
+	switch content {
+	default:
+		fmt.Println("Unknown language")
+	case "python":
+		fmt.Println("An interpreted Lanauage")
+	case "go":
+		fmt.Println("A compiled language")
+	}
+
+}
