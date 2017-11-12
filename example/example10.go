fbd37099b064bb02b1fe1aeccc7b2e7358f71e32
stvenyin <893692456@qq.com>
2017-11-12 09:57:15 +0800

    example10

---
+++ example/example10.go
@@ -0,0 +1,30 @@
+// test project main.go
+package main
+
+import (
+	"fmt"
+)
+
+func findEvildoer(name string) string {
+	var evildoer string
+	for _, r := range name {
+		switch {
+		case r >= '\u0041' && r <= '\u005a': // a-z
+		case r >= '\u0061' && r <= '\u0071': //A-Z
+		case r >= '\u4e00' && r <= '\u9fbf': //ÖÐÎÄ×Ö·û
+			goto L3
+		default:
+			evildoer = string(r)
+			goto L2
+		}
+	}
+
+L2:
+	fmt.Printf("The frist evildoer of name '%s' is '%s'\n", name, evildoer)
+L3:
+	return evildoer
+}
+
+func main() {
+	findEvildoer("kkkkk@@@@")
+}
