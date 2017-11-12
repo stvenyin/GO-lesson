3d3f8428328cc2d0d4bd5d83c185e5f1e7c52a11
stvenyin <893692456@qq.com>
2017-11-12 10:59:34 +0800

    example11

---
+++ example/example11.go
@@ -0,0 +1,16 @@
+// test project main.go
+package main
+
+import (
+	"fmt"
+)
+
+func PrintNumbers() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Printf("%d\n", i)
+	}
+}
+
+func main() {
+	PrintNumbers()
+}
