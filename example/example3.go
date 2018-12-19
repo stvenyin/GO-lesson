0c6b0bab0b1d05593afa053ae4aefe114c1a5505
stvenyin <32395964+stvenyin@users.noreply.github.com>
2018-12-19 11:12:33 +0800

    arr

--- example/example3.go
+++ example/example3.go
@@ -6,7 +6,9 @@ import (
 )
 
 func main() {
-
+	
+	//数组示例 var []array{}
+	
 	array := [8]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}
 	fmt.Println([...]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}[1])
 	fmt.Println(len([8]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}))
