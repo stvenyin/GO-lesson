d89b3d10be98063b9f466b9c37f233188002175d
stvenyin <32395964+stvenyin@users.noreply.github.com>
2018-12-19 11:24:59 +0800

    Update example3.go

--- example/example3.go
+++ example/example3.go
@@ -14,8 +14,8 @@ func main() {
 	fmt.Println([...]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}[2])
 	fmt.Println([...]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}[3])
 	fmt.Println([...]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}[5])
-	fmt.Println(len([8]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}))
+	fmt.Println(len([900]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"})) //初始化方式略有不同
+	fmt.Println(cap([900]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}))
 	fmt.Println(array[3])
 
 }
-
