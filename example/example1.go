cb83b48d3e5c6192ee2673d9a3d0a20a25c19d4f
stvenyin <32395964+stvenyin@users.noreply.github.com>
2018-12-19 11:02:17 +0800

    Update example1.go

--- example/example1.go
+++ example/example1.go
@@ -10,7 +10,7 @@ func init() {
 	fmt.Printf("Map: %v\n", m)
 	//通过调用runtime包的代码获取当前机器所运行的操作系统及计算机框架
 	//而后通过fmt包的Sprintf方法进行字符串格式化并赋值给变量info
-	info = fmt.Sprintf("OS:%s,Arch:%s", runtime.GOOS, runtime.GOARCH)
+	info = fmt.Sprintf("OS:%s,Arch:%s GOROOT:%s", runtime.GOOS, runtime.GOARCH,runtime.GOROOT())
 }
 
 var m map[int]string = map[int]string{1: "A", 2: "B", 3: "C"}
