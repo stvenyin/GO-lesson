eac3c88a7d06671a5119ab5d952552bdb61e82f5
stvenyin <32395964+stvenyin@users.noreply.github.com>
2018-12-19 11:44:02 +0800

    Update example4.go

--- example/example4.go
+++ example/example4.go
@@ -10,6 +10,21 @@ func main() {
 	
 	//
 	//slice 切片
+	//
+	s :=[] int {1,2,3 } 
+	//直接初始化切片，[]表示是切片类型，{1,2,3}初始化值依次是1,2,3.其cap=len=3
+	//s := arr[:] 
+	//初始化切片s,是数组arr的引用
+	//s := arr[startIndex:endIndex] 
+	//将arr中从下标startIndex到endIndex-1 下的元素创建为一个新的切片
+	//s := arr[startIndex:] 
+	//缺省endIndex时将表示一直到arr的最后一个元素
+	//s := arr[:endIndex] 
+	//缺省startIndex时将表示从arr的第一个元素开始
+	//s1 := s[startIndex:endIndex] 
+	//通过切片s初始化切片s1
+	//s :=make([]int,len,cap) 
+	//通过内置函数make()初始化切片s,[]int 标识为其元素类型为int的切片
 
 	fmt.Println(len([]string{"Go", "Python", "Java", "C", "C++", "PHP"}))
 	fmt.Println(cap([]string{"Go", "Python", "Java", "C", "C++", "PHP"}))
