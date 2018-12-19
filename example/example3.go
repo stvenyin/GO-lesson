2436f7c98b96f2de249eae9cf08521de4344dab3
stvenyin <32395964+stvenyin@users.noreply.github.com>
2018-12-19 11:19:04 +0800

    Update example3.go

--- example/example3.go
+++ example/example3.go
@@ -6,13 +6,16 @@ import (
 )
 
 func main() {
-	
+
 	//数组示例 var []array{}
-	
+
 	array := [8]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}
 	fmt.Println([...]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}[1])
+	fmt.Println([...]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}[2])
+	fmt.Println([...]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}[3])
+	fmt.Println([...]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}[5])
 	fmt.Println(len([8]string{1: "Go", "Python", 4: "Java", "C", "C++", "PHP"}))
 	fmt.Println(array[3])
-	
-	
+
 }
+
